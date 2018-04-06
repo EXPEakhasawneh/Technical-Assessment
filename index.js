d2c51472b71002676f727d0f1f5856a9d05c490f
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-06 18:35:19 +0300

    Add API function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
@@ -10,6 +11,20 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.get('/api/offers', (req, res) => {
+  let params = {
+    scenario: 'deal-finder',
+    page: 'foo',
+    uid: 'foo',
+    productType: 'Hotel'
+  }
+  let API = 'https://offersvc.expedia.com/offers/v2/getOffers';
+
+  return axios.get(API, {params: params})
+    .then(response => res.send(response.data))
+    .catch(error => console.dir(error));
+})
 
 app.get('/', (req, res) => {
   res.render('dist/index.html');
