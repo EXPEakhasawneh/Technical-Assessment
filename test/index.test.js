0ac5405d36dce2f00c7b1dab2559dc94f31afcd0
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-08 22:51:53 +0300

    Add app listner for test server

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,9 @@ const app = require('../index.js');
 
 describe('Test the root path', () => {
   test('It should response the GET method', () => {
+    app.listen(process.env.PORT || 3001)
     return request(app).get('/')
       .then(response => {
-        // console.log(response.text.conatins('app'))
         expect(response.statusCode).toBe(200);
       });
   });
