8b24551881e222392b3f43136ed25a457095a245
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-09 12:14:49 +0300

    Play around test

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,13 @@
 const request = require('supertest');
 const app = require('../index.js');
-app.listen(3000, () => {
-  console.log(`Server works on: 3000`);
-});
+
 
 describe('Test the root path', () => {
-  test('It should response the GET method', () => {
+  test('It should response the GET method', (done) => {
     return request(app).get('/')
       .then(response => {
         expect(response.statusCode).toBe(200);
+        done();
       });
   });
 
