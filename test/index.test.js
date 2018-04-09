40c253600bce395227eb53c95845d299d3baf6e7
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-09 12:22:54 +0300

    another try

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const app = require('../index.js');
 
 describe('Test the root path', () => {
   test('It should response the GET method', (done) => {
-    return request(app).get('/')
+    request(app).get('/')
       .then(response => {
         expect(response.statusCode).toBe(200);
         done();
