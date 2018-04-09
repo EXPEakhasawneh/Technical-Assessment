71805583f9b1216019da8f32619d5bfd31215a2e
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-09 12:27:55 +0300

    Remove GET method test, just test API call

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('../index.js');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', (done) => {
-    request(app).get('/')
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
+describe('Test Suite', () => {
+  // test('It should response the GET method', (done) => {
+  //   request(app).get('/')
+  //     .then(response => {
+  //       expect(response.statusCode).toBe(200);
+  //       done();
+  //     });
+  // });
 
   test('It should response API request', () => {
     return request(app).get('/api/offers')
