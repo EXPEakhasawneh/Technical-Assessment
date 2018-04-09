fd269274fd9e9e82f01d9e54d11b638ea1a3ca91
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-09 02:01:02 -0700

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,17 @@
 const request = require('supertest');
 const app = require('../index.js');
-app.listen(3000, () => {
-  console.log(`Server works on: 3000`);
-});
 
 describe('Test the root path', () => {
   test('It should response the GET method', (done) => {
     return request(app).get('/')
-      .expect(200, done);
+      .expect(200);
+    done.end();
   });
 
   test('It should response API request', () => {
     return request(app).get('/api/offers')
       .then(response => {
+      console.log(response)
         expect(response.body.offers).toBeDefined();
       });
   });
