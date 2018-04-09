4daba07542fd61541e5473f0a2440dcb373b3eb0
Ahmad Khasawneh <akhasawneh@expedia.com>
2018-04-09 11:16:49 +0300

    Listen to the same port

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../index.js');
-app.listen(5000, () => {
-  console.log(`Server works on: 5000`);
+app.listen(3000, () => {
+  console.log(`Server works on: 3000`);
 });
 
 describe('Test the root path', () => {
